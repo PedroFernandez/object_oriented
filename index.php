5b78c85506fd5d826635366fea1eb4cf811acf68
PedroFernandez <fdez.pedro@gmail.com>
2018-02-25 12:28:32 +0100

    use getters instead of object property

--- index.php
+++ index.php
@@ -65,7 +65,10 @@ if (isset($_GET['error'])) {
                 <tbody>
                     <?php foreach ($ships as $ship): ?>
                         <tr>
-                            <td><?php echo $ship->getNameAndSpecs($ship); ?></td></tr>
+                            <td><?php echo $ship->getName(); ?></td>
+                            <td><?php echo $ship->getWeaponPower(); ?></td>
+                            <td><?php echo $ship->getJediFactor(); ?></td>
+                            <td><?php echo $ship->getStrength(); ?></td>
                     <?php endforeach; ?>
                 </tbody>
             </table>

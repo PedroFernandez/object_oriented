9d06c710fbc26f4866c060c7431f5043c8217597
PedroFernandez <fdez.pedro@gmail.com>
2018-02-25 13:10:31 +0100

    pass argument to the constructor

--- functions.php
+++ functions.php
@@ -8,29 +8,25 @@ function get_ships()
     $ships = [];
 
     /** @var Ship $ship1 */
-    $ship1 = new Ship();
-    $ship1->setName('Jedi Starfighter');
+    $ship1 = new Ship('Jedi Starfighter');
     $ship1->setWeaponPower(5);
     $ship1->setJediFactor(15);
     $ship1->setStrength(30);
     $ships['jedi_starfighter'] = $ship1;
 
-    $ship2 = new Ship();
-    $ship2->setName('Cloakshape Fighter');
+    $ship2 = new Ship('Cloakshape Fighter');
     $ship2->setWeaponPower(2);
     $ship2->setJediFactor(2);
     $ship2->setStrength(70);
     $ships['cloakshape_fighter'] = $ship2;
 
-    $ship3 = new Ship();
-    $ship3->setName('Super Star Destroyer');
+    $ship3 = new Ship('Super Star Destroyer');
     $ship3->setWeaponPower(2);
     $ship3->setJediFactor(2);
     $ship3->setStrength(70);
     $ships['super_star_destroyer'] = $ship3;
 
-    $ship4 = new Ship();
-    $ship4->setName('RZ1 A Wing Interceptor');
+    $ship4 = new Ship('RZ1 A Wing Interceptor');
     $ship4->setWeaponPower(2);
     $ship4->setJediFactor(2);
     $ship4->setStrength(70);

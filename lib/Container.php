7d870cdce2ab620e988feb11016ea6a226f7f567
PedroFernandez <fdez.pedro@gmail.com>
2018-03-25 17:54:59 +0200

    force single objects on container

--- lib/Container.php
+++ lib/Container.php
@@ -4,6 +4,10 @@ class Container
 {
     private $pdo;
 
+    private $shipLoader;
+
+    private $battleManager;
+
     public function __construct($configuration)
     {
         $this->configuration = $configuration;
@@ -11,14 +15,33 @@ class Container
 
     public function getPdo()
     {
-        if ($this->pdo === null) {
+        if ($this->pdo == null) {
             $this->pdo = new PDO(
                 $this->configuration['db_dns'],
                 $this->configuration['db_user'],
                 $this->configuration['db_pass']
             );
+            $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         }
 
         return $this->pdo;
+    }
+
+    public function getShipLoader()
+    {
+        if ($this->shipLoader === null) {
+            $this->shipLoader = new ShipLoader($this->getPdo());
+        }
+
+        return $this->shipLoader;
+    }
+
+    public function getBattleManager()
+    {
+        if ($this->battleManager === null) {
+            $this->battleManager = new BattleManager();
+        }
+
+        return $this->battleManager;
     }
 }

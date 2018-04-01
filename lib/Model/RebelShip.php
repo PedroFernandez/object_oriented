b8a9e44640643fd7f5aef610548f08e98ab9a0c9
PedroFernandez <fdez.pedro@gmail.com>
2018-04-01 09:46:35 +0200

    calling parent class mehods

--- lib/Model/RebelShip.php
+++ lib/Model/RebelShip.php
@@ -22,22 +22,9 @@ class RebelShip extends Ship
 
     public function getNameAndSpecs($useShortFormat = false)
     {
-        if ($useShortFormat) {
-            return sprintf(
-                '%s: %s/%s/%s (Rebel)',
-                $this->getName(),
-                $this->getWeaponPower(),
-                $this->getJediFactor(),
-                $this->getStrength()
-            );
-        } else {
-            return sprintf(
-                '%s: w:%s, j:%s, s:%s (Rebel)',
-                $this->getName(),
-                $this->getWeaponPower(),
-                $this->getJediFactor(),
-                $this->getStrength()
-            );
-        }
+        $nameAndSpecs = parent::getNameAndSpecs($useShortFormat);
+        $nameAndSpecs .= ' (Rebel)';
+
+        return $nameAndSpecs;
     }
 }

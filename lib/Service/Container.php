b168d083ba6e809c40648d23e395fab59a8341f2
PedroFernandez <fdez.pedro@gmail.com>
2018-04-09 22:08:39 +0200

    namespaces and php core classes

--- lib/Service/Container.php
+++ lib/Service/Container.php
@@ -2,9 +2,6 @@
 
 namespace Service;
 
-use PDO;
-use Service\BattleManager;
-
 class Container
 {
     private $pdo;
@@ -23,12 +20,12 @@ class Container
     public function getPdo()
     {
         if ($this->pdo == null) {
-            $this->pdo = new PDO(
+            $this->pdo = new \PDO(
                 $this->configuration['db_dns'],
                 $this->configuration['db_user'],
                 $this->configuration['db_pass']
             );
-            $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $this->pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
         }
 
         return $this->pdo;
@@ -46,8 +43,8 @@ class Container
     public function getPdoShipStorage()
     {
         if ($this->pdoShipStorage === null) {
-//            $this->pdoShipStorage = new PdoShipStorage($this->getPdo());
-            $this->pdoShipStorage = new JsonFileShipStorage(__DIR__ . '/../../resources/ships.json');
+            $this->pdoShipStorage = new PdoShipStorage($this->getPdo());
+//            $this->pdoShipStorage = new JsonFileShipStorage(__DIR__ . '/../../resources/ships.json');
         }
 
         return $this->pdoShipStorage;
--- lib/Service/PdoShipStorage.php
+++ lib/Service/PdoShipStorage.php
@@ -4,13 +4,13 @@ namespace Service;
 
 class PdoShipStorage implements ShipStorageInterface
 {
-    /** @var PDO */
+    /** @var \PDO */
     private $pdo;
 
     /**
-     * @param PDO $pdo
+     * @param \PDO $pdo
      */
-    public function __construct(PDO $pdo)
+    public function __construct(\PDO $pdo)
     {
         $this->pdo = $pdo;
     }
@@ -19,7 +19,7 @@ class PdoShipStorage implements ShipStorageInterface
     {
         $statement = $this->pdo->prepare('SELECT * from ship');
         $statement->execute();
-        $shipsArray = $statement->fetchAll(PDO::FETCH_ASSOC);
+        $shipsArray = $statement->fetchAll(\PDO::FETCH_ASSOC);
 
         return $shipsArray;
     }
@@ -29,7 +29,7 @@ class PdoShipStorage implements ShipStorageInterface
         $pdo = $this->pdo;
         $statement = $pdo->prepare('SELECT * from ship WHERE id = :id');
         $statement->execute(array('id' => $id));
-        $shipArray = $statement->fetch(PDO::FETCH_ASSOC);
+        $shipArray = $statement->fetch(\PDO::FETCH_ASSOC);
 
         if (!$shipArray) {
             return null;

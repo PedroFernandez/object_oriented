2a1c1156ff95a0bbd5a9cf41df2fa4f5b14be822
PedroFernandez <fdez.pedro@gmail.com>
2018-04-09 22:39:39 +0200

    prepare bootstrap for autoload

--- bootstrap.php
+++ bootstrap.php
@@ -1,28 +1,9 @@
 <?php
 
-require_once __DIR__ . '/lib/Model/AbstractShip.php';
-require_once __DIR__. '/lib/Model/Ship.php';
-require_once __DIR__. '/lib/Model/RebelShip.php';
-require_once __DIR__. '/lib/Model/BattleResult.php';
-require_once __DIR__. '/lib/Model/BrokenShip.php';
-//require __DIR__. '/lib/Service/BattleManager.php';
-require_once __DIR__. '/lib/Service/ShipLoader.php';
-require_once __DIR__. '/lib/Service/Container.php';
-require_once __DIR__.'/lib/Service/ShipStorageInterface.php';
-require_once __DIR__.'/lib/Service/PdoShipStorage.php';
-require_once __DIR__.'/lib/Service/JsonFileShipStorage.php';
+require __DIR__.'/vendor/autoload.php';
 
 $configuration = [
     'db_dns' => 'mysql:host=localhost;dbname=oo_battle',
     'db_user' => 'root',
     'db_pass' => 'root'
 ];
-
-spl_autoload_register(function ($className) {
-
-    $path = __DIR__. '/lib/' . str_replace('\\', '/', $className) . '.php';
-
-    if (file_exists($path)) {
-        require $path;
-    };
-});

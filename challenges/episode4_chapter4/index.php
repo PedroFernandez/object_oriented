200f12625ba34bf32fec7d5e1af68bc825c9571e
PedroFernandez <fdez.pedro@gmail.com>
2018-04-14 19:17:52 +0200

    challenges - small change

--- challenges/episode4_chapter4/index.php
+++ challenges/episode4_chapter4/index.php
@@ -4,7 +4,9 @@ require __DIR__ . '/PlanetRenderer.php';
 require __DIR__ . '/PlanetInterface.php';
 require __DIR__ . '/RandomlyColoredPlanet.php';
 
-$planet = new Model\Planet\RandomlyColoredPlanet('0969F9', 'F96909');
+use Model\Planet\RandomlyColoredPlanet;
+
+$planet = new RandomlyColoredPlanet('0969F9', 'F96909');
 
 $renderer = new PlanetRenderer();
 echo $renderer->render($planet);

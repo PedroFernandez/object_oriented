f6ee5932fcb36347b75c3cf49e52128799913ac6
PedroFernandez <fdez.pedro@gmail.com>
2018-04-15 17:59:57 +0200

    namespaces and core php classes

--- challenges/episode4_chapter6/Model/Planet/SolidPlanet.php
+++ challenges/episode4_chapter6/Model/Planet/SolidPlanet.php
@@ -8,10 +8,13 @@ class SolidPlanet implements PlanetInterface
 
     private $hexColor;
 
-    public function __construct($radius, $hexColor)
+    private $dayLengthInHours;
+
+    public function __construct($radius, $hexColor, $dayLengthInHours = 24)
     {
         $this->radius = $radius;
         $this->hexColor = $hexColor;
+        $this->dayLengthInHours = $dayLengthInHours;
     }
 
     public function getRadius()
@@ -22,5 +25,16 @@ class SolidPlanet implements PlanetInterface
     public function getHexColor()
     {
         return $this->hexColor;
+    }
+
+    public function getDateTimeOneDayFromNow()
+    {
+        // ...
+        // just an example of how to use the DateTime object
+        // $tomorrow = new DateTime();
+        // $tomorrow->modify('+24 hours');
+        $date = new \DateTime();
+
+        return $date->modify('+'.$this->dayLengthInHours.' hours');
     }
 }
--- challenges/episode4_chapter6/index.php
+++ challenges/episode4_chapter6/index.php
@@ -1,22 +1,17 @@
 <?php
 
 spl_autoload_register(function ($class) {
-
     require __DIR__ . '/' . str_replace('\\', '/', $class) . '.php';
 });
 
-use Service\PlanetRenderer;
-use Model\Planet\GasPlanet;
 use Model\Planet\SolidPlanet;
-use Model\Planet\RandomlyColoredPlanet;
 
-$planet1 = new GasPlanet(GasPlanet::MATERIAL_AMMONIA, 49);
-$planet2 = new SolidPlanet(27, '353535');
-$planet3 = new RandomlyColoredPlanet('0969F9', 'F96909');
-$planet4 = new SolidPlanet(45, 'F96909');
+$planet = new SolidPlanet(100, 'ff00ff', 12);
+$tomorrow = $planet->getDateTimeOneDayFromNow();
 
-$renderer = new PlanetRenderer();
-echo $renderer->render($planet1);
-echo $renderer->render($planet2);
-echo $renderer->render($planet3);
-echo $renderer->render($planet4);
+?>
+
+<h3>
+    1 day from now:
+    <?php echo $tomorrow->format('Y-m-d H:i:s'); ?>
+</h3>
